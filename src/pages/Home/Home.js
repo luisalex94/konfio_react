5b5c23bd2e4e0de54e0c2053ade7f5544cfb552e
AlejandroJR <luisalex944@gmail.com>
2024-10-19 19:02:38 -0600

    general enhancement of the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,28 +3,24 @@ import axios from 'axios';
 import konfioLogo from '../../assets/konfio_logo_cuadrado.png';
 import './Home.css';
 import { json, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
     const location = useLocation(); // Hook para acceder a la información pasada
     const { userData } = location.state || {}; // Extrae userData de location.state
 
-    console.log('userData:', userData);
-
-    const [balance, setBalance] = useState(null);
+    const [balance, setBalance] = useState(0.0);
     const [transactions, setTransactions] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [loadingSignOut, setLoadingSignOut] = useState(false);
 
-    // inicia un array vacio   
-    let trans_test = [];
-
-    console.log('userData:', userData);
+    const navigate = useNavigate();
 
     const payload = {
         account: userData.account,
     };
 
     useEffect(() => {
-        // Función para obtener el saldo
         const fetchBalance = async () => {
             try {
                 const response = await axios.post('https://dw73vr1mj3.execute-api.us-east-1.amazonaws.com/prod/get-balance', payload, {
@@ -39,7 +35,6 @@ function Home() {
             }
         };
 
-        // Función para obtener los movimientos
         const fetchTransactions = async () => {
             try {
                 const response = await axios.post('https://dw73vr1mj3.execute-api.us-east-1.amazonaws.com/prod/get-movements', payload, {
@@ -47,32 +42,27 @@ function Home() {
                         'Content-Type': 'application/json'
                     }
                 });
+                const movements = JSON.parse(response.data[0].movements)['movements'];
 
-                const movements = JSON.parse(response.data[0].movements)['movements']; // Parsea el JSON de la respuesta
-                console.log('Response movements 0001:', movements);
-
-                trans_test = movements;
-
-                console.log('Response movements XX01: ', trans_test);
-
-                console.log('Response movements XX02: ', trans_test[0].date);
-
-                // imprime con un map el array de objetos
-                trans_test.map((transaction, index) => {
-                    console.log('Response movements XX03:', transaction);
+                // Ver el tipo y si dice deposit cambiarlo a Deposito y si dice charge cambiarlo a Cargo
+                movements.forEach((movement) => {
+                    if (movement.type === 'deposit') {
+                        movement.type = 'Depósito';
+                    } else if (movement.type === 'charge') {
+                        movement.type = 'Cargo';
+                    }
+                    if (movement.amount < 0) {
+                        movement.amount = -movement.amount;
+                    }
                 });
 
+                console.log('Response transactions:', movements);
                 setTransactions(movements);
-                // imprime tipo de dato
-                console.log('Response movements 0002:', typeof transactions);
-                console.log('Response movements 0003:', transactions);
-
-                
             } catch (error) {
                 console.error('Error fetching transactions:', error);
             }
 
-            
+
         };
 
         fetchBalance();
@@ -80,14 +70,15 @@ function Home() {
         setLoading(false); // Simulando que ha terminado de cargar después de obtener los datos
     }, [userData]);
 
-    // Usar useEffect para ver el valor actualizado de transactions
-    useEffect(() => {
-        console.log('Transactions updated:', transactions);
-    }, [transactions]); // Este efecto se ejecutará cada vez que `transactions` cambie
+    const handleLogout = async (event) => {
+        event.preventDefault();
+        setLoadingSignOut(true)
+        setTimeout(() => {
+            setLoadingSignOut(false);
+            //navigate('/');
+        }, 2000)
 
 
-    const handleLogout = () => {
-        // Lógica de cierre de sesión
         console.log("Cerrando sesión...");
     };
 
@@ -110,25 +101,29 @@ function Home() {
             <header>
                 <img src={konfioLogo} alt="Konfio Logo" className="konfio-logo" />
                 <div className="header-buttons">
-                    <button onClick={handleRequestCredit}>Solicitar Crédito</button>
-                    <button onClick={handleSettings}>Configuración</button>
-                    <button onClick={handleLogout}>Cerrar Sesión</button>
+                    <button className='fixed-button-a' onClick={handleRequestCredit}>Solicitar Crédito</button>
+                    <button className='fixed-button-a' onClick={handleSettings}>Configuración</button>
+                    {loadingSignOut ? (
+                        <div className="loader"></div>
+                    ) : (
+                        <button className='fixed-button-a' onClick={handleLogout}>Cerrar sesión</button>
+                    )}
                 </div>
             </header>
-
             <main>
                 <h1>Bienvenido, {userData.name}</h1> {/* Nombre del usuario */}
                 <section className="balance-section">
                     <h2>Tu saldo</h2>
-                    <p>${balance}</p> {/* Saldo */}
+                    <p>${balance.toFixed(2)}</p>
                 </section>
 
                 <section className="transactions-section">
                     <h2>Tus movimientos</h2>
                     <ul>
-                        {transactions.map((transaction, index) => (
+                        {transactions.slice().reverse().map((transaction, index) => (
                             <li key={index}>
-                                <p>{transaction.date}: {transaction.description} - ${transaction.amount}</p>
+                                <p>{transaction.concept}: ${transaction.amount.toFixed(2)} - {transaction.date}</p>
+                                <p>{transaction.type} - {transaction.id}</p>
                             </li>
                         ))}
                     </ul>
