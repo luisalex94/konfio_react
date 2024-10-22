2d765a0d896248a194172aaff32bd50f960adb97
AlejandroJR <luisalex944@gmail.com>
2024-10-22 15:21:57 -0600

    modify classNames products

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -13,34 +13,33 @@ function Products() {
 
   return (
 
-    <div className="products-services">
+    <div className="products-products-services">
       <div className="products-header-content">
         <nav>
-          <div className="columns">
-            <div className="spacer"></div>
-            <button onClick={handleBack} className='back-button'>Regresar</button>
-            <h1 className='head-text'>Productos y Servicios</h1>
-            <div className="spacer"></div>
+          <div className="products-columns">
+            <div className="products-spacer"></div>
+            <h1 className="products-head-text">Productos y Servicios</h1>
+            <div className="products-spacer"></div>
           </div>
         </nav>
-        <img src={konfioLogo} alt="Konfio Logo" className="konfio-logo" />
+        <img src={konfioLogo} alt="Konfio Logo" className="products-konfio-logo" />
 
       </div>
 
-      <div className="grid-container">
-        <div className="product-box">
+      <div className="products-grid-container">
+        <div className="products-product-box-01">
           <h2>Prestamos empresariales</h2>
           <p>Prespuesto para tus grandes ideas.</p>
         </div>
-        <div className="product-box">
+        <div className="products-product-box-02">
           <h2>Micropréstamos</h2>
           <p>Préstamos pequeños para quienes apenas comienzan.</p>
         </div>
-        <div className="product-box">
+        <div className="products-product-box-03">
           <h2>Hipotecas empresariales</h2>
           <p>Integramos las hipotecas con productos bancarios para una máxima flexibilidad</p>
         </div>
-        <div className="product-box">
+        <div className="products-product-box-04">
           <h2>Terminales y otros</h2>
           <p>Por que lo pequeño no es lo menos importante, las terminales y otros servicios te dan ofrecen seriedad y seguridad ante tus clientes.</p>
         </div>
